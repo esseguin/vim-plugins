1f691e5e8abb072b4e3e138785117cf0a9b4667a
Evan Seguin <seguin.evan@gmail.com>
2021-03-05 15:37:01 -0500

    Better init

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.config/nvim/init.vim ~/.vimrc
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 nvim +PluginInstall +qall
