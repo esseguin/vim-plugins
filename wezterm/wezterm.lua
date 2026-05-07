f197475ba9ef73f3e3d3c1e08122213adaea8124
Evan Seguin <seguin.evan@gmail.com>
2026-05-07 11:25:27 -0400

    Add wezterm, fish, git, gh configs
    First batch of non-nvim configs. gh/hosts.yml excluded via .gitignore
    to keep auth tokens out of source control.

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+---@type Wezterm
+local wezterm = require("wezterm")
+
+---@type Config
+local config = wezterm.config_builder()
+
+config.color_scheme = "Nord (Gogh)"
+
+config.keys = {
+	-- splits
+	{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- close pane
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+}
+
+-- Pane navigation + resize that's aware of nvim splits: when nvim is the
+-- foreground process the keypress is forwarded to nvim (handled by
+-- smart-splits.nvim); otherwise wezterm moves between its own panes.
+-- CTRL+h/j/k/l = navigate, OPT+h/j/k/l = resize.
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	modifiers = { move = "CTRL", resize = "META" },
+})
+
+return config
